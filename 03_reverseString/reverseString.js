e2f94a4fdeaa8e3683f562cf3bad3de1c64f0b31
Samuel Adu <samuelnkadu@gmail.com>
2022-06-17 01:32:07 -0400

    Completed 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = [];
+    let reverseString = '';
 
+    for (let i = 0; i < string.length; i++) {
+        splitString.unshift(string.charAt(i));
+    }
+
+    for (let i = 0; i < splitString.length; i++) {
+        reverseString += splitString[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
