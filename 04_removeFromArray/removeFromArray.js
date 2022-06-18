570c6937851e2e31a64ee5bdd02639e62ab8a99b
Samuel Adu <samuelnkadu@gmail.com>
2022-06-18 03:37:30 -0400

    Completed exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    args.map(remove => {
+        if (array.includes(remove)) {
+            array.splice(array.indexOf(remove), 1);
+        }
+    })
+
+    return array;
 };
 
 // Do not edit below this line
