a4a41b8742553f6ee387d87f8037d1a7c929cc12
Samuel Adu <samuelnkadu@gmail.com>
2022-06-19 02:41:11 -0400

    Completed exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let add = 0;
+	array.forEach(number => {
+    add += number;
+  })
+  return add;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiply = 1;
+	array.forEach(number => {
+    multiply *= number;
+  })
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fact = 1;
+	for (i = number; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
