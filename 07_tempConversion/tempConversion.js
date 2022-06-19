f35dbdbd64e746650ebbdb7ac41eb55ab2af1f05
Samuel Adu <samuelnkadu@gmail.com>
2022-06-19 01:53:09 -0400

    Completed exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let converted = ((temp - 32) * 5/9).toFixed(1);
+  return Number(converted)
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let converted =  (temp * 9/5 + 32).toFixed(1);
+  return Number(converted);
 };
 
 // Do not edit below this line
