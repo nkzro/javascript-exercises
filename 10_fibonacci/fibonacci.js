cac90b28764ccadb0d3b2a94f7024291db9b2456
Samuel Adu <samuelnkadu@gmail.com>
2022-06-19 06:03:33 -0400

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+//The argument n refers to the nth position of the fibonacci sequence.
+const fibonacci = function(n) {
+    Number(n);
+    let start = 1;
+    let end = 1;
+    let next = 0;
+    
+    if (n < 0) {
+        return 'OOPS';
+    }
+    else if (n <= 2) {
+        return 1;
+    }
+    else if (n > 2) {
+        for (let i = 2; i < n; i++) {
+            fib(start, end)
+            start = end;
+            end = next;
+            //console.log(next)
+        }
+        
+        return next;
+    }
 
+    function fib(start, end) {
+        next = start + end;
+        return next;
+    }
 };
 
 // Do not edit below this line
