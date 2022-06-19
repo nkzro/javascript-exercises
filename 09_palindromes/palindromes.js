8c094d2f0f7749381f0521bb5a77a0e6315603c7
Samuel Adu <samuelnkadu@gmail.com>
2022-06-19 05:27:19 -0400

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let letters = /^[a-zA-Z]+$/;
+    let stringArray = [];
+    let reverseStringArray = [];
 
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i).match(letters)) {
+            stringArray.push(string.charAt(i).toLowerCase());
+            reverseStringArray.unshift(string.charAt(i).toLowerCase());
+        }
+    }
+    
+    let isPalindrome = stringArray.every(function(element, index) {
+        return element === reverseStringArray[index];
+    });
+
+    if (isPalindrome) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
