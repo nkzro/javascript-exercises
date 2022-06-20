3e8952e8b1596e0d38e23f30f69ff6ba8ffd1e55
Samuel Adu <samuelnkadu@gmail.com>
2022-06-19 21:20:25 -0400

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = people.map(element => {
 
+        if (element.yearOfDeath !== undefined) {
+            return element.yearOfDeath - element.yearOfBirth;
+        }
+        else {
+            let currentDate = new Date();
+            console.log(currentDate.getFullYear())
+            return currentDate.getFullYear() - element.yearOfBirth;
+        }
+    });
+
+    let oldest = age.indexOf(Math.max(...age));
+
+    return people[oldest];
 };
 
 // Do not edit below this line
